532d51bbf4df5deb1b67e181f8bca5ae1ea84f20
Eddie Melendez <exmelendez@gmail.com>
2019-08-12 09:53:53 -0400

    added delete counter functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,10 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    id: this.props.id,
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   handleIncrement = product => {
-    console.log(product);
     this.setState({ value: this.state.value + 1 });
   };
 
@@ -20,6 +18,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
