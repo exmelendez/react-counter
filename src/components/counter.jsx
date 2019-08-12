3d8b21a67867ff91bc5f1d0d196d1e7f914845ff
Eddie Melendez <exmelendez@gmail.com>
2019-08-12 08:47:59 -0400

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    id: this.props.id,
+    value: this.props.value
+  };
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ item: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
